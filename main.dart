3a2dd02b7105cd76020ba9858856248151a7b421
Zaink2298 <zainuddinkhan2298@gmail.com>
2025-07-26 03:57:06 +0500

    learn conditional statement

---
+++ main.dart
@@ -0,0 +1,14 @@
+void main() {
+  print("Hello World");
+  int totalAssignment=5;
+  int minAssignmentDone=4;
+  int assignmentDone=4;
+  //bool islastClassAttended=true  
+  if(assignmentDone>=minAssignmentDone){
+   print("Enter in Class");
+  }
+  else{
+    print("class se bahir hoga please");
+  }
+
+}
